bac8805e39d257b7806ec5a21c4a0f55d8205c61
Artur_A <arthursae@mail.ru>
2022-12-14 05:33:02 +0500

    Comment added

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -2,7 +2,7 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-const int min = -100, max = 100;
+const int min = -100, max = 100; // Set MIN and MAX range for PRG-number
 
 void OutputTheResultOfCalculation(int[] numbersArray, int sum)
 {
